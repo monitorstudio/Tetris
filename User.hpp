79649213e6548cea34325675f1478e61a12b699e
monitorstudio <monitorstudioworker@gmail.com>
2016-01-09 02:16:23 +0800

    Update User.hpp

--- User.hpp
+++ User.hpp
@@ -1,50 +1,25 @@
 #ifndef USER_H
 #define USER_H
 
+#include <linux/input.h>
+
 typedef enum KeyName
 {
-	K_UP_P		=	458834,
-	K_UP_C		=	103,
-	K_UP_R		=	1,
-	K_DOWN_P	=	458833,
-	K_DOWN_C	=	108,
-	K_DOWN_R	=	2,
-	K_LEFT_P	=	458832,
-	K_LEFT_C	=	105,
-	K_LEFT_R	=	3,
-	K_RIGHT_P	=	458831,
-	K_RIGHT_C	=	106,
-	K_RIGHT_R	=	4,
-	K_SPACE_P	=	458796,
-	K_SPACE_C	=	57,
-	K_SPACE_R	=	5,
-	K_LSHIFT_P	=	458977,
-	K_LSHIFT_C	=	42,
-	K_LSHIFT_R	=	6,
-	K_RSHIFT_P	=	458981,
-	K_RSHIFT_C	=	54,
-	K_RSHIFT_R	=	7,
-	K_LCTRL_P	=	458976,
-	K_LCTRL_C	=	29,
-	K_LCTRL_R	=	8,
-	K_RCTRL_P	=	488980,
-	K_RCTRL_C	=	97,
-	K_RCTRL_R	=	9,
-	K_P_P		=	458771,
-	K_P_C		=	25,
-	K_P_R		=	10,
-	K_Q_P		=	458772,
-	K_Q_C		=	16,
-	K_Q_R		=	11,
-	K_S_P		=	458774,
-	K_S_C		=	31,
-	K_S_R		=	12
+	KEY_DOWN_P	= 1,
+	KEY_DOWN_C	= 2,
+	KEY_DOWN_R	= 3,
+	KEY_LEFT_P	= 4,
+	KEY_LEFT_C	= 5,
+	KEY_LEFT_R	= 6,
+	KEY_RIGHT_P	= 7,
+	KEY_RIGHT_C	= 8,
+	KEY_RIGHT_R	= 9
 } KeyName;
 
 class User
 {
 public:
-	static KeyName getInput(FILE *);
+	static int getInput(FILE *);
 private:
 };
 
