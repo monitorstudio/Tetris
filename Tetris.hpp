b7b10bfb14898427a8c89cf923d4583cb638661b
monitorstudio <monitorstudioworker@gmail.com>
2016-01-20 15:17:31 +0800

    Update Tetris.hpp

--- Tetris.hpp
+++ Tetris.hpp
@@ -5,15 +5,16 @@
 
 #include "Block.hpp"
 
-#define	WHITE	"\033[1;37;47m"
-#define	CYAN	"\033[0;36;46m"
-#define	YELLOW	"\033[1;33;43m"
-#define	GREEN	"\033[0;32;42m"
-#define	RED	"\033[0;31;41m"
-#define	ORANGE	"\033[0;33;43m"
-#define	BLUE	"\033[0;34;44m"
-#define	PURPLE	"\033[1;35;45m"
-#define	GRAY	"\033[1;30;40m"
+#define	WHITE	255
+#define	CYAN	187
+#define	YELLOW	238
+#define	GREEN	426
+#define	RED		460
+#define	ORANGE	358
+#define	BLUE	409
+#define	PURPLE	477
+#define	GRAY	392
+#define BLACK	256
 
 class BlockHolder
 {
