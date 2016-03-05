89de88c4173cf9501081a1ff4fe55171e23a5e60
monitorstudio <monitorstudioworker@gmail.com>
2016-03-05 20:55:24 +0800

    Update Tetris.hpp

--- Tetris.hpp
+++ Tetris.hpp
@@ -1,44 +1,75 @@
 #ifndef TETRIS_HPP_
 #define TETRIS_HPP_
 
+#include <iostream>
 #include <deque>
+#include <cstdio>
+#include <cstddef>
 
-#include "Block.hpp"
+#define	WHITE   "\033[1;37;47m"
+#define BGWHITE "\033[0;37;47m"
+#define	CYAN    "\033[0;36;46m"
+#define	YELLOW  "\033[1;33;43m"
+#define	GREEN   "\033[0;32;42m"
+#define	RED     "\033[0;31;41m"
+#define	ORANGE  "\033[0;33;43m"
+#define	BLUE    "\033[0;34;44m"
+#define	PURPLE  "\033[0;35;45m"
+#define	GRAY    "\033[1;30;40m"
+#define DEFAULT "\033[0m"
 
-#define	WHITE	"\033[1;37;47m"
-#define	CYAN	"\033[0;36;46m"
-#define	YELLOW	"\033[1;33;43m"
-#define	GREEN	"\033[0;32;42m"
-#define	RED	"\033[0;31;41m"
-#define	ORANGE	"\033[0;33;43m"
-#define	BLUE	"\033[0;34;44m"
-#define	PURPLE	"\033[1;35;45m"
-#define	GRAY	"\033[1;30;40m"
-
-class BlockHolder
+inline void hide_cursor(void)
 {
-public:
-	BlockHolder(void);
-	void hold(block_t &);
-	void clear(void);
-	void reset(void);
-private:
-	block_t _hold;
-};
+        std::cout << "\033[?25l";
+        std::fflush(stdout);
+}
 
-class BlockGenerator
+inline void show_cursor(void)
 {
-public:
-	BlockGenerator(void);
-	BlockGenerator(int, int);
-	block_t getBlock(void);
-	void reset(void);
-private:
-	int _y, _x;
-	int _pos;
-	block_t _nextBlock(void);
-	std::deque<block_t> _buffer;
-};
+        std::cout << "\033[?25h";
+        std::fflush(stdout);
+}
 
-#endif	// TETRIS_HPP_
+inline void goto_yx(size_t y, size_t x)
+{
+        std::cout << "\033[" << y << ";" << x << "H";
+        std::fflush(stdout);
+}
 
+inline void clear(void)
+{
+        std::cout << "\033[2J";
+        std::fflush(stdout);
+}
+
+// FIXME clear_line NOT work
+inline void clear_line(void)
+{
+        std::cout << DEFAULT << "\033[K";
+        std::fflush(stdout);
+}
+
+// FIXME clear_line NOT work
+inline void clear_line(std::size_t y)
+{
+        goto_yx(y, 0);
+        clear_line();
+}
+
+inline void print_cell(const char *color)
+{
+        std::cout << color << "██";
+        std::fflush(stdout);
+}
+
+inline void print_cell(const char *color, std::size_t length)
+{
+        std::cout << color;
+        for(std::size_t i = 0; i < length; i++)
+        {
+                std::cout << "█";
+        }
+        std::fflush(stdout);
+}
+
+#endif  // TETRIS_HPP_
