b7eb4a6d2965ffefca91b02f56fcb2e88e17e1ae
monitorstudio <monitorstudioworker@gmail.com>
2016-03-07 03:55:46 +0800

    Added files via upload

--- Array.hpp
+++ Array.hpp
@@ -6,75 +6,45 @@
 #include <vector>
 
 template <typename T>
-class Array
+class Array : public std::vector<T>
 {
 public:
-        Array(void)
-        {
-        }
-
         T &operator[](size_t i)
         {
-                if(i >= _array.size())
-                        _array.resize(i + 1);
+                if(i >= this->size())
+                        this->resize(i + 1);
 
-                return _array[i];
+                return *(this->begin() + i);
         }
 
         bool operator==(Array<T> &array)
         {
-                if(_array.size() != array.size())
+                if(this->size() != array.size())
                         return false;
 
-                for(size_t i = 0, size = _array.size(); i < size; i++)
-                        if(_array[i] != array[i])
+                for(size_t i = 0, size = this->size(); i < size; i++)
+                        if((*this)[i] != array[i])
                                 return false;
 
                 return true;
         }
-
         bool operator!=(Array<T> &array)
         {
-                if(_array.size() != array.size())
+                if(this->size() != array.size())
                         return true;
 
-                for(size_t i = 0, size = _array.size(); i < size; i++)
-                        if(_array[i] != array[i])
+                for(size_t i = 0, size = this->size(); i < size; i++)
+                        if((*this)[i] != array[i])
                                 return true;
 
                 return false;
         }
 
-        Array<T> operator=(Array<T> array)
-        {
-                _array.resize(array.size());
-
-                for(size_t i = 0, size = _array.size(); i < size; i++)
-                        _array[i] = array[i];
-
-                return array;
-        }
-
-        size_t size(void)
-        {
-                return _array.size();
-        }
-
-        void resize(size_t s)
-        {
-                _array.resize(s);
-        }
-
-        void clear(void)
-        {
-                _array.clear();
-        }
-
         bool find(T &t)
         {
-                for(size_t i = 0, size = _array.size(); i < size; i++)
+                for(size_t i = 0, size = this->size(); i < size; i++)
                 {
-                        if(_array[i] == t)
+                        if((*this)[i] == t)
                                 return true;
                 }
 
@@ -83,21 +53,19 @@ public:
 
         void for_each(std::function<void(T &, size_t)> map)
         {
-                for(size_t i = 0, size = _array.size(); i < size; i++)
+                for(size_t i = 0, size = this->size(); i < size; i++)
                 {
-                        map(_array[i], i);
+                        map((*this)[i], i);
                 }
         }
 
         void for_each(std::function<void(T &)> map)
         {
-                for(size_t i = 0, size = _array.size(); i < size; i++)
+                for(size_t i = 0, size = this->size(); i < size; i++)
                 {
-                        map(_array[i]);
+                        map((*this)[i]);
                 }
         }
-private:
-        std::vector<T> _array;
 };
 
 #endif        // ARRAY_HPP_
